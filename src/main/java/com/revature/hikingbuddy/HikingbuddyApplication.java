a51603c79c43a26152b77abc9d10be22aea563d2
Patrick Powers <patrickpowers221@gmail.com>
2023-06-04 17:32:19 -0400

    Had to recreate this branch because it kept including my .DS_Store file

---
+++ src/main/java/com/revature/hikingbuddy/HikingbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.revature.hikingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingbuddyApplication.class, args);
+	}
+
+}
